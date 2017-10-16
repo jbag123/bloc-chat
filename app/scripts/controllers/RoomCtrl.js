efaeac5e2b7aa7a206dffe4d0ea89501745f22f6
jbag123 <joshcone@googlemail.com>
2017-10-16 18:30:59 +0100

    commiting chat room title output

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,14 +1,19 @@
 (function() {
         function  RoomCtrl(Room, $uibModal) {
                 this.roomList = Room.all;
-
+                
                 this.open = function() {
                         $uibModal.open({
                               templateUrl: '/templates/modal.html',
-                              controller: 'ModalCtrl as modal',
-                });
+                              controller: 'ModalCtrl as modal'
+                       });
+               };
+
+                this.setCurrentRoom = function(room) {
+                        //currentRoom = room;
+                        console.log(room.$value);
+                }
         }
-}
 
         angular
                 .module('blocChat')
